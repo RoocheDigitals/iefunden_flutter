7d32515a4033cd24fa81b4d09faa3ba451bb834b
Rooche Digital <hello@rooche.net>
2023-02-13 11:33:30 +0800

    update sign in service url

--- lib/services/cppa.dart
+++ lib/services/cppa.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:http/http.dart' as http;
 import 'package:iefunden/commons/constants.dart';
 
 class CPPAService {
   Future<dynamic> signIn(String email, String password) async {
+    log("${API_AUTH_BASE_URL}login/");
     final response = await http.post(
-      Uri.parse(API_AUTH_BASE_URL),
+      Uri.parse("${API_AUTH_BASE_URL}login/"),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
