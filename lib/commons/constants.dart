e91823fdffa774a79358b6868340bb8c76871b5e
Rooche Digital <hello@rooche.net>
2023-02-13 11:04:21 +0800

    added new constant variables and added http package

--- lib/commons/constants.dart
+++ lib/commons/constants.dart
@@ -1,1 +1,4 @@
 String APP_TITLE = "ieFundEn";
+String API_BASE_URL =
+    "https://ylhuwjxxu3.execute-api.us-east-1.amazonaws.com/dev/";
+String API_AUTH_BASE_URL = "${API_BASE_URL}auth/";
--- lib/services/cppa.dart
+++ lib/services/cppa.dart
@@ -1,13 +1,12 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:iefunden/commons/constants.dart';
 
 class CPPAService {
   Future<dynamic> signIn(String email, String password) async {
     final response = await http.post(
-      Uri.parse(
-        'https://ylhuwjxxu3.execute-api.us-east-1.amazonaws.com/dev/auth/login/',
-      ),
+      Uri.parse(API_AUTH_BASE_URL),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
